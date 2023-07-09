a060265cd63e93e0b5fde98b3a16efcb660bd475
gustavo viana <gustavo.viana@fraudkeeper.com>
2023-07-09 13:33:42 -0300

    dfs graph list

---
+++ src/dfs_graph_list.rs
@@ -0,0 +1,90 @@
+struct GraphEdge {
+    to: usize,
+    weight: usize,
+}
+type WeightedAdjacencyList = Vec<Vec<GraphEdge>>;
+
+fn walk(
+    graph: &WeightedAdjacencyList,
+    curr: usize,
+    needle: usize,
+    seen: &mut Vec<bool>,
+    path: &mut Vec<usize>,
+) -> bool {
+    if seen[curr] {
+        return false;
+    }
+
+    seen[curr] = true;
+
+    // RECURSE
+    // pre
+    path.push(curr);
+    if curr == needle {
+        return true;
+    }
+    // recurse
+    let list = &graph[curr];
+
+    for i in 0..list.len() {
+        let edge = &list[i];
+
+        if walk(graph, edge.to, needle, seen, path) {
+            return true;
+        }
+    }
+
+    // post
+    path.pop();
+
+    false
+}
+
+fn dfs(graph: &WeightedAdjacencyList, source: usize, needle: usize) -> Option<Vec<usize>> {
+    let mut seen = vec![false; graph.len()];
+    let mut path: Vec<usize> = vec![];
+
+    walk(graph, source, needle, &mut seen, &mut path);
+
+    if path.is_empty() {
+        return None;
+    }
+
+    Some(path)
+}
+
+#[cfg(test)]
+mod test_dfs_graph_list {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        //     >(1)<--->(4) ---->(5)
+        //    /          |       /|
+        // (0)     ------|------- |
+        //    \   v      v        v
+        //     >(2) --> (3) <----(6)
+        let list2: WeightedAdjacencyList = vec![
+            vec![
+                GraphEdge { to: 1, weight: 3 },
+                GraphEdge { to: 2, weight: 1 },
+            ],
+            vec![GraphEdge { to: 4, weight: 1 }],
+            vec![GraphEdge { to: 3, weight: 7 }],
+            vec![],
+            vec![
+                GraphEdge { to: 1, weight: 1 },
+                GraphEdge { to: 3, weight: 5 },
+                GraphEdge { to: 5, weight: 2 },
+            ],
+            vec![
+                GraphEdge { to: 2, weight: 18 },
+                GraphEdge { to: 6, weight: 1 },
+            ],
+            vec![GraphEdge { to: 3, weight: 1 }],
+        ];
+
+        assert_eq!(dfs(&list2, 0, 6), Some(vec![0, 1, 4, 5, 6]));
+        assert_eq!(dfs(&list2, 6, 0), None);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod bt_post_order;
 mod bt_pre_order;
 mod bubble_sort;
 mod depht_first_search_on_bst;
+mod dfs_graph_list;
 mod linear_search;
 mod linked_list;
 mod maze_solver;
